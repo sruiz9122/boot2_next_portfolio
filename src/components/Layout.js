8b960df9179be064ddb020753b18b6f4b9a368de
Sergio Andres Ruiz Bustos <67773662+sruiz9122@users.noreply.github.com>
2024-07-29 18:57:20 -0500

    Repo completo Ejercicio 29072024

---
+++ src/components/Layout.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react'
+import Navbar from './Navbar'
+import Nprogress  from 'nprogress';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+
+const Layout = ({ children, footer = true }) => {
+
+  const router = useRouter();
+
+  useEffect(() => {
+
+    // Efecto para manejar cambios de ruta
+    const handleRouteChange = () => {
+      Nprogress.start();
+    };
+
+    const handleChangeComplete = () => {
+      Nprogress.done();
+    };
+
+    const handleChangeError = () => {
+      Nprogress.done();
+    };
+
+    // Escucha el inicio de cambio de ruta
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    // Escucha el final exitoso de cambio de ruta
+    router.events.on("routeChangeComplete", handleChangeComplete);
+
+    // Escucha el error en el cambio deruta
+    router.events.on("routeChangeError", handleChangeError);
+    
+    return () => {
+      // Limpia el event listener al desmontar el componente
+      router.events.off("routeChangeStart", handleRouteChange);
+      router.events.off("routeChangeComplete", handleChangeComplete);
+      router.events.off("routeChangeError", handleChangeError);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Navbar />
+
+      {children}
+
+      { footer && (
+        <footer className="bg-dark text-light text-center">
+        <div className="container p-4">
+          <h1>&copy; Sergio Ruiz Portfolio</h1>
+          <p>2024 - {new Date().getFullYear()}</p>
+          <p>All rights Reserved.</p>
+        </div>
+      </footer>
+      )}
+    </>
+  )
+};
+
+Layout.propTypes = {
+  children: PropTypes.node,
+  footer: PropTypes.bool
+};
+
+export default Layout;
---
+++ src/pages/profile.js
@@ -0,0 +1,115 @@
+
+export const skills = [
+  {
+    skill: "JavaScript",
+    percentage: 90,
+  },
+  {
+    skill: "TypeScript",
+    percentage: 80,
+  },
+  {
+    skill: "Node.js",
+    percentage: 98,
+  },
+  {
+    skill: "Next.js",
+    percentage: 100,
+  },
+  {
+    skill: "MongoDB",
+    percentage: 60,
+  },
+  {
+    skill: "React.js",
+    percentage: 100,
+  },
+  {
+    skill: "Bootstrap",
+    percentage: 100,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software developer at Microsoft",
+    description:
+      "Microsoft.com",
+    from: 2000,
+    to: 2005,
+  },
+  {
+    title: "Software developer at Google",
+    description:
+      "Google.com",
+    from: 2005,
+    to: 2007,
+  },
+  {
+    title: "Software developer at SpaceX",
+    description:
+      "SpaceX.com",
+    from: 2010,
+  },
+];
+
+export const projects = [
+  {
+    name: "Awesome Website 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 4",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 5",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 6",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
